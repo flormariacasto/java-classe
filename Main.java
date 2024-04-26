10c045028c29739db05facc5918cdc5dfa850480
flormariacasto <162711011+flormariacasto@users.noreply.github.com>
2024-04-26 13:12:21 -0300

    Create Main.java
    heloisa 3A segunda nota

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String marca = "Toyota";
+        String modelo = "Corolla";
+        String ano = "2022";
+
+        System.out.println("A marca do carro é:" + marca);
+        System.out.println("O modelo do carro é:" + modelo);
+        System.out.println("O ano do carro é:" + ano);
+    }
